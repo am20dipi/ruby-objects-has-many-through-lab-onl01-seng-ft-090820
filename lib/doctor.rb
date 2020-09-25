49495c5235abc0e40633371fca53f8ac81666151
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-25 00:42:08 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -25,7 +25,7 @@ class Doctor
   end
   
   def patients
-    appointments.all.collect {|appointment| appointment.patients}
+    Appointment.all.collect {|appointment| appointment.patients}
   end
   
 end
