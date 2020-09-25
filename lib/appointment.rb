98461b0e1cff88c03f4aacf6401eac4084416486
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-25 00:23:22 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,25 @@
+class Appointment
+  attr_accessor :date, :patient, :doctor
+  @@all = []
+  
+  def self.all
+    @@all 
+  end
+  
+  def initialize(date="", patient="", doctor="")
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def patient
+    #belongs_to :patient
+    self.patient
+  end
+  
+  def doctor
+    #belongs_to :doctor
+    self.doctor
+  end
+end
---
+++ lib/patient.rb
@@ -0,0 +1,30 @@
+class Patient
+  attr_accessor :name, :appointment, :doctor
+  
+  @@all = []
+  
+  def self.all
+    @@all 
+  end
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(date, doctor)
+    newappt = Appointment.new(date, doctor, self)
+    newappt
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+  
+  def doctors
+    appointments.map do |appointment| 
+      appointment.doctors == self
+    end
+  end
+    
+end
